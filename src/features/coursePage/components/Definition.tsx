fb27b262c50853337a016d01ed3b4cfd114e057c
Etsku520 <eetu.huovila@gmail.com>
2019-04-03 15:39:47 +0300

    add automatic numbering fot theorem

--- src/features/coursePage/components/Definition.tsx
+++ src/features/coursePage/components/Definition.tsx
@@ -4,7 +4,7 @@ const Definition = (props: any) => {
   return (
     <div>
       <div className="definition-header">
-        <h3 className="definition-header-content"> {props.header} </h3>
+        <h3 className="definition-header-content"> Määritelmä: {props.header} </h3>
       </div>
 
       <div className="definition-content">
