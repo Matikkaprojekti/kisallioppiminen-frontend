6045341a61320799b100271d4d6395955d8e6337
hjoonas <heik.joonas@gmail.com>
2019-04-28 17:01:46 +0300

    Teoria button toggles material theory visibility

--- src/features/baseComponents/NavBottom.tsx
+++ src/features/baseComponents/NavBottom.tsx
@@ -23,23 +23,37 @@ export function NavBottom() {
       })
     }
   }
-  /*const [openTheory, setOpenTheory] = useState(false)
+  const [openTheory, setOpenTheory] = useState(true)
   const changeTheoryVisibility = () => {
-    setOpenExercise(!openTheory)
-    if (openExercise) {
-      Array.from(document.getElementsByClassName('theory-text')).forEach(element => {
-        if (element !== undefined) {
-          element.className = 'theory-text-hidden'
+    setOpenTheory(!openTheory)
+    if (openTheory) {
+      Array.from(document.getElementsByClassName('chapter-content')).forEach(element => {
+        if (element.children !== undefined) {
+          Array.from(element.children).forEach(e => {
+            const tags = ['DIV', 'P', 'UL', 'OL', 'IMG', 'SPAN', 'H3', 'H2', 'H1']
+            const classNames = ['close-chapter', 'exercise', 'exercise-content', 'exercise-hidden', 'exercise-content-hidden']
+            if (tags.includes(e.tagName) && !classNames.includes(e.className)) {
+              if ((e.children === undefined || e.children.length === 0) || (e.children !== undefined && e.children.length > 0 && !classNames.includes(e.children[0].className))) {
+                e.setAttribute('style', 'display: none;')
+              }
+            }
+          })
         }
       })
     } else {
-      Array.from(document.getElementsByClassName('theory-text-hidden')).forEach(element => {
-        if (element !== undefined) {
-          element.className = 'theory-text'
+      Array.from(document.getElementsByClassName('chapter-content')).forEach(element => {
+        if (element.children !== undefined) {
+          Array.from(element.children).forEach(e => {
+            const tags = ['DIV', 'P', 'UL', 'OL', 'IMG', 'SPAN', 'H3', 'H2', 'H1']
+            const classNames = ['close-chapter', 'exercise', 'exercise-content', 'exercise-hidden', 'exercise-content-hidden']
+            if (tags.includes(e.tagName) && !classNames.includes(e.className)) {
+              e.setAttribute('style', 'display: block;')
+            }
+          })
         }
       })
     }
-  }*/
+  }
   const [openAll, setOpenAll] = useState(false)
   const changeAllVisibility = () => {
     setOpenAll(!openAll)
@@ -67,7 +81,6 @@ export function NavBottom() {
           <span className="toggle" />
         </label>
       </span>
-      {/*
       <span className="navigatorBottom-item">
         Teoria
         <label className="switch">
@@ -75,7 +88,6 @@ export function NavBottom() {
           <span className="toggle" />
         </label>
       </span>
-      */}
       <span className="navigatorBottom-item">
         Avaa kaikki
         <label className="switch">
